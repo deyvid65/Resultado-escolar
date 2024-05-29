91ad083fbf883fbeae3d3048864e3a9eb1db966b
deyvid65 <deyvdalysson@gmail.com>
2024-05-29 09:37:37 -0300

    resultado escolar

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -5,7 +5,10 @@ public class ResultadoEscolar {
 
         if(nota >= 7)
            System.out.println("Aprovado");
-    
+        
+        else if(nota >=5 && nota <=7)
+           System.out.println("Recuperação");
+
         else
           System.out.println("reprovado");
 
